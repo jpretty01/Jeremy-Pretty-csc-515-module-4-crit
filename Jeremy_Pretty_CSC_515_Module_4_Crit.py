922d80c7b44dc4881d3a5f2ddb5a273700a86481
Jeremy D. Pretty <70484698+jpretty01@users.noreply.github.com>
2023-03-05 10:07:42 -0600

    Update
    Update!

--- Jeremy_Pretty_CSC_515_Module_4_Crit.py
+++ Jeremy_Pretty_CSC_515_Module_4_Crit.py
@@ -12,38 +12,34 @@ img = cv2.imread(Mod_Img, 0)
 kernel_sizes = [3, 5, 7]
 
 # Define sigma values for Gaussian filter
-sigmas = [1, 2]
+sigma_values = [1, 2]
 
 # Define filter types
-filters = {'Mean': cv2.blur, 'Median': cv2.medianBlur, 'Gaussian (sigma=1)': lambda img: cv2.GaussianBlur(img, (0,0), sigmaX=1),
-           'Gaussian (sigma=2)': lambda img: cv2.GaussianBlur(img, (0,0), sigmaX=2)}
+filters = {'Mean': lambda img, ksize: cv2.blur(img, (ksize,ksize)),
+           'Median': lambda img, ksize: cv2.medianBlur(img, ksize),
+           'Gaussian 1': lambda img, ksize: cv2.GaussianBlur(img, (ksize,ksize), sigmaX=sigma_values[0]),
+           'Gaussian 2': lambda img, ksize: cv2.GaussianBlur(img, (ksize,ksize), sigmaX=sigma_values[1])}
 
 # Apply filters for different kernel sizes and sigma values
 results = {}
 for ksize in kernel_sizes:
     for filter_name, filter_func in filters.items():
         if 'Gaussian' in filter_name:
-            for sigma in sigmas:
-                filtered_img = filter_func(img, (ksize, ksize), sigmaX=sigma)
-                results[(ksize, filter_name + f' (sigma={sigma})')] = filtered_img
+            filtered_img = filter_func(img, ksize)
         else:
-            filtered_img = filter_func(img, (ksize, ksize))
-            results[(ksize, filter_name)] = filtered_img
+            filtered_img = filter_func(img, ksize)
+        results[(ksize, filter_name)] = filtered_img
 
 # Plot the results
-fig, axs = plt.subplots(len(kernel_sizes), len(filters), figsize=(12,8))
+fig, axs = plt.subplots(len(kernel_sizes), len(filters), figsize=(16,12))
 for i, ksize in enumerate(kernel_sizes):
     for j, filter_name in enumerate(filters.keys()):
-        if 'Gaussian' in filter_name:
-            for sigma in sigmas:
-                filtered_img = results[(ksize, filter_name + f' (sigma={sigma})')]
-                axs[i,j].imshow(filtered_img, cmap='gray')
-                axs[i,j].set_title(f'{filter_name} {ksize}x{ksize} (sigma={sigma})')
-        else:
-            filtered_img = results[(ksize, filter_name)]
-            axs[i,j].imshow(filtered_img, cmap='gray')
-            axs[i,j].set_title(f'{filter_name} {ksize}x{ksize}')
+        filtered_img = results[(ksize, filter_name)]
+        axs[i,j].imshow(filtered_img, cmap='gray')
+        axs[i,j].set_title(f'{filter_name} {ksize}x{ksize}')
         axs[i,j].axis('off')
     axs[i,0].set_ylabel(f'{ksize}x{ksize}', fontsize=12)
+axs[0,2].set_title(f'{filters["Gaussian 1"].__name__} {kernel_sizes[0]}x{kernel_sizes[0]} sigma={sigma_values[0]}')
+axs[0,3].set_title(f'{filters["Gaussian 2"].__name__} {kernel_sizes[0]}x{kernel_sizes[0]} sigma={sigma_values[1]}')
 plt.tight_layout()
 plt.show()
